69df0487a1dbc7196ea352da60bb0b145368fcbf
Dima_Os <osipovdima1989@gmail.com>
2020-09-20 13:20:19 +0300

    added chandging logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import './styles.css';
+import { colors } from './js/colors';
+
+const refs = {
+  start: document.querySelector('button[data-action="start"]'),
+  stop: document.querySelector('button[data-action="stop"]'),
+  body: document.querySelector('body'),
+};
+
+let startIntervalChanging = null;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const changeColor = color => {
+  refs.body.setAttribute('style', `background-color:${color}`);
+};
+
+const onStartHendler = () => {
+  startIntervalChanging = setInterval(() => {
+    changeColor(colors[randomIntegerFromInterval(0, colors.length - 1)]);
+  }, 1000);
+  refs.start.setAttribute('disabled', 'disabled');
+};
+
+const onStopHendler = () => {
+  clearInterval(startIntervalChanging);
+  refs.start.removeAttribute('disabled');
+};
+
+refs.start.addEventListener('click', onStartHendler);
+refs.stop.addEventListener('click', onStopHendler);
